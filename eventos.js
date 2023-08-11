ba68e1d40c361b46067894f0570d24b3378241cf
Pablo Guala <gualapab@gmail.com>
2023-08-11 00:18:58 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -1,11 +1,15 @@
-function saludar() {
-  alert("¡Hola!");
-}
-
-function soydiv() {
-  alert("¡Hola! Soy el div");
-}
-
 document.addEventListener("DOMContentLoaded", () => {
+  const hola = function hola() {
+    document.querySelector("div").removeEventListener("click", soydiv);
+    alert("¡Hola!");
+    setTimeout(() => {
+      document.querySelector("div").addEventListener("click", soydiv);
+    }, 10);
+  };
+  document.querySelector("button").addEventListener("click", hola);
+
+  const soydiv = function () {
+    alert("¡Hola! Soy el div");
+  };
   document.querySelector("div").addEventListener("click", soydiv);
 });
